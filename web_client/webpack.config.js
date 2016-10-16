50e1693831a7b364c5c4a73b3b98344c4749d9b9
Andrew Klotz <agc.klotz@gmail.com>
2016-10-16 00:19:55 -0400

    added webpack dashboard, awyeah!

--- web_client/webpack.config.js
+++ web_client/webpack.config.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -25,7 +26,8 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
-    })
+    }),
+    new DashboardPlugin()
   ],
   module: {
     loaders: [
